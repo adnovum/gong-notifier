b2d99bc6ba1020b4e432be9578946cb12083aa90
Sandro Heinzelmann <sandro.heinzelmann@gmail.com>
2020-06-07 14:28:07 +0200

    Clean up plugin settings

--- gong-notifier-email/src/main/java/ch/adnovum/gong/notifier/email/PluginSettings.java
+++ gong-notifier-email/src/main/java/ch/adnovum/gong/notifier/email/PluginSettings.java
@@ -1,14 +1,16 @@
 package ch.adnovum.gong.notifier.email;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import ch.adnovum.gong.notifier.PluginSettingsBase;
+import ch.adnovum.gong.notifier.events.HistoricalEvent;
 import ch.adnovum.gong.notifier.go.api.SettingsField;
 import ch.adnovum.gong.notifier.util.GongUtil;
 
 public class PluginSettings extends PluginSettingsBase {
-	
+
+	private static final String DEFAULT_EVENTS = "broken, fixed, failed";
 	private static final String DEFAULT_SMTP_HOST = "localhost";
 	private static final String DEFAULT_SMTP_PORT = "25";
 	private static final String DEFAULT_SENDER_EMAIL = "noreply@localhost.com";
@@ -18,6 +20,8 @@ public class PluginSettings extends PluginSettingsBase {
 	
 	public static final Map<String, SettingsField> FIELD_CONFIG = new HashMap<>();
 	static {
+		FIELD_CONFIG.put("defaultEvents", new SettingsField("Default notification events", DEFAULT_EVENTS, false, false,
+				0));
 		FIELD_CONFIG.put("smtpHost", new SettingsField("Smtp Host", DEFAULT_SMTP_HOST, false, false, 0));
 		FIELD_CONFIG.put("smtpPort", new SettingsField("Smtp Port", DEFAULT_SMTP_PORT, false, false, 0));
 		FIELD_CONFIG.put("senderEmail", new SettingsField("Sender E-mail", DEFAULT_SENDER_EMAIL, true, false, 0));
@@ -25,12 +29,28 @@ public class PluginSettings extends PluginSettingsBase {
 		FIELD_CONFIG.put("bodyTemplate", new SettingsField("E-mail body template", DEFAULT_BODY_TEMPLATE, false, false, 0));
 	}
 
+	private String defaultEvents = DEFAULT_EVENTS;
 	private String smtpHost = DEFAULT_SMTP_HOST;
 	private String smtpPort = DEFAULT_SMTP_PORT;
 	private String senderEmail = DEFAULT_SENDER_EMAIL;
 	private String subjectTemplate = DEFAULT_SUBJECT_TEMPLATE;
 	private String bodyTemplate = DEFAULT_BODY_TEMPLATE;
 	private String timezone = DEFAULT_TIMEZONE;
+
+	public String getDefaultEvents() {
+		return valueOrDefault(defaultEvents, DEFAULT_EVENTS);
+	}
+
+	public Set<HistoricalEvent> getDefaultEventsSet() {
+		return Arrays.stream(getDefaultEvents().split("\\s*,\\s*"))
+				.map(String::toUpperCase)
+				.map(HistoricalEvent::valueOf)
+				.collect(Collectors.toSet());
+	}
+
+	public void setDefaultEvents(String defaultEvents) {
+		this.defaultEvents = defaultEvents;
+	}
 	
 	public String getSmtpHost() {
 		return valueOrDefault(smtpHost, DEFAULT_SMTP_HOST);
@@ -86,42 +106,21 @@ public class PluginSettings extends PluginSettingsBase {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		if (!super.equals(o)) {
-			return false;
-		}
-
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
 		PluginSettings that = (PluginSettings) o;
-
-		if (getSmtpHost() != null ? !getSmtpHost().equals(that.getSmtpHost()) : that.getSmtpHost() != null) {
-			return false;
-		}
-		if (getSmtpPort() != null ? !getSmtpPort().equals(that.getSmtpPort()) : that.getSmtpPort() != null) {
-			return false;
-		}
-		if (getSenderEmail() != null ? !getSenderEmail().equals(that.getSenderEmail()) : that.getSenderEmail() != null) {
-			return false;
-		}
-		if (getSubjectTemplate() != null ? !getSubjectTemplate().equals(that.getSubjectTemplate())
-				: that.getSubjectTemplate() != null) {
-			return false;
-		}
-		return getBodyTemplate() != null ? getBodyTemplate().equals(that.getBodyTemplate()) : that.getBodyTemplate() == null;
+		return Objects.equals(getDefaultEvents(), that.getDefaultEvents()) &&
+				Objects.equals(getSmtpHost(), that.getSmtpHost()) &&
+				Objects.equals(getSmtpPort(), that.getSmtpPort()) &&
+				Objects.equals(getSenderEmail(), that.getSenderEmail()) &&
+				Objects.equals(getSubjectTemplate(), that.getSubjectTemplate()) &&
+				Objects.equals(getBodyTemplate(), that.getBodyTemplate()) &&
+				Objects.equals(getTimezone(), that.getTimezone());
 	}
 
 	@Override
 	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + (getSmtpHost() != null ? getSmtpHost().hashCode() : 0);
-		result = 31 * result + (getSmtpPort() != null ? getSmtpPort().hashCode() : 0);
-		result = 31 * result + (getSenderEmail() != null ? getSenderEmail().hashCode() : 0);
-		result = 31 * result + (getSubjectTemplate() != null ? getSubjectTemplate().hashCode() : 0);
-		result = 31 * result + (getBodyTemplate() != null ? getBodyTemplate().hashCode() : 0);
-		return result;
+		return Objects.hash(super.hashCode(), getDefaultEvents(), getSmtpHost(), getSmtpPort(), getSenderEmail(), getSubjectTemplate(), getBodyTemplate(), getTimezone());
 	}
 }
